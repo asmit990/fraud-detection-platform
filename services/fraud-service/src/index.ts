6cb4eb1bee8a30e34c6fec798921e3264886349a
asmit990 <asmitpandeyw4354@gmail.com>
2026-06-13 13:29:46 +0530

    added: fraud services

--- services/fraud-service/src/index.ts
+++ services/fraud-service/src/index.ts
@@ -1,1 +1,39 @@
 import "dotenv/config"
+import express from "express";
+import pool from "./db"
+import { connectConsumer } from "./kafka"
+
+const app = express()
+
+app.use(express.json())
+
+
+const PORT = process.env.PORT ?? 3002
+
+
+app.get("/health", (_req, res) => {
+  res.json({ status: "ok hai darling" });
+});
+
+
+
+async function start() {
+    try {
+        await pool.connect() 
+
+        console.log("Postgres connected")
+
+        await connectConsumer()
+        console.log("Kafka consumer connected")
+       
+        app.listen(PORT, () => {
+            console.log(`Fraud service running on port ${PORT}`);
+        })
+
+    } catch (error) {
+        console.error("Error connecting to Postgres:", error)
+    }
+}
+
+
+start()
